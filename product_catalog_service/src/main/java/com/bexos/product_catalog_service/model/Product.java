77b2ee46f538eac706ed4c4049e5d4944b2c8cbb
Kjeff24 <jeffreyarthur123@gmail.com>
2025-04-08 07:23:31 +0000

    Added product and category model

---
+++ product_catalog_service/src/main/java/com/bexos/product_catalog_service/model/Product.java
@@ -0,0 +1,28 @@
+package com.bexos.product_catalog_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    @ManyToOne
+    private Category category;
+}
