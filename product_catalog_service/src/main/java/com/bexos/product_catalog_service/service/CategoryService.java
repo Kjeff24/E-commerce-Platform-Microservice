d4a1f4be71fd72cad41ab235d7851bf1f315ff82
Kjeff24 <jeffreyarthur123@gmail.com>
2025-04-09 12:55:21 +0000

    Added category service

--- product_catalog_service/src/main/java/com/bexos/product_catalog_service/service/CategoryService.java
+++ product_catalog_service/src/main/java/com/bexos/product_catalog_service/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.bexos.product_catalog_service.service;
 
+import com.bexos.product_catalog_service.dto.CategoryRequest;
+import com.bexos.product_catalog_service.model.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    Category addCategory(CategoryRequest categoryRequest);
+
+    List<Category> getAllCategory();
 }
--- product_catalog_service/src/main/java/com/bexos/product_catalog_service/service/impl/CategoryServiceImpl.java
+++ product_catalog_service/src/main/java/com/bexos/product_catalog_service/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,31 @@
 package com.bexos.product_catalog_service.service.impl;
 
+import com.bexos.product_catalog_service.dto.CategoryRequest;
+import com.bexos.product_catalog_service.exception.BadRequestException;
+import com.bexos.product_catalog_service.model.Category;
+import com.bexos.product_catalog_service.repository.CategoryRepository;
 import com.bexos.product_catalog_service.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Category addCategory(CategoryRequest categoryRequest) {
+        if(categoryRepository.existsByName(categoryRequest.getName())) {
+            throw new BadRequestException("Category already exists");
+        }
+        return categoryRepository.save(modelMapper.map(categoryRequest, Category.class));
+    }
+
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
 }
